96e4388a9129d57e43d29ce4935e7154438c8563
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2022-03-06 23:03:56 -0600

    Crando Autentication Basic con pasportjs

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const { config } = require('./src/config/');
+
 const applicationTestApi = require('./src/routes/applicationtest');
 const todosApi = require('./src/routes/todo-list');
+const authApi = require('./src/routes/auth');
+
+const {
+    logErrors,
+    errorHandler,
+    wrapErrors
+  } = require('./src/utils/middleware/errorHandlers.js');
+
+ const notFoundHandler = require('./src/utils/middleware/notFoundHandler');
+
 
 app.use(cors());
 
 app.use(express.json());
 
+authApi(app);
 applicationTestApi(app);
 todosApi(app);
 
-app.listen(3002, () => {
-    console.log('Server is up on port 3002!');
+
+//Catch 404
+app.use(notFoundHandler);
+
+//Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+
+app.listen(config.port, function() {
+    console.log(`Listening http://localhost:${config.port}`);
 });
--- src/config/index.js
+++ src/config/index.js
@@ -8,12 +8,12 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   
-  /*cors: process.env.CORS,
+  cors: process.env.CORS,
   defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
   defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
   authJwtSecret: process.env.AUTH_JWT_SECRET,
   publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
-  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,*/
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 };
 
 module.exports = { config };
